d4aab4bbcc0fb7cfd017ee1ebe470f74665f0e9f
Alexey Kravets <kravecoleksiy@gmail.com>
2025-06-04 18:22:42 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead tr');
+const tfoot = document.querySelector('tfoot tr');
+const table = document.querySelector('table');
+const rows = table.querySelectorAll('tbody tr');
+
+const newTh = document.createElement('th');
+
+newTh.textContent = 'Position';
+thead.insertBefore(newTh.cloneNode(true), thead.lastElementChild);
+tfoot.insertBefore(newTh.cloneNode(true), tfoot.lastElementChild);
+
+rows.forEach((row) => {
+  const cells = row.querySelectorAll('td');
+  const secondCell = cells[1];
+  const newCell = document.createElement('td');
+
+  newCell.textContent = secondCell.textContent;
+
+  row.insertBefore(newCell, row.lastElementChild);
+});
